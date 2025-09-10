8c08b13c6746163c19fab5241116ba00006ef5e7
Bruno Piovan <bpiovan90@gmail.com>
2025-09-10 14:57:16 -0300

    criado controller para transferencia

--- src/main/java/com/utfpr/trustpay/controller/TransferenciaController.java
+++ src/main/java/com/utfpr/trustpay/controller/TransferenciaController.java
@@ -1,9 +1,25 @@
 package com.utfpr.trustpay.controller;
 
+import com.utfpr.trustpay.model.dtos.TransferenciaRequestDTO;
+import com.utfpr.trustpay.model.dtos.TransferenciaResponseDTO;
+import com.utfpr.trustpay.service.TransferenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transferencia")
 public class TransferenciaController {
+
+    @Autowired
+    private TransferenciaService transferenciaService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody TransferenciaRequestDTO transferenciaRequestDTO) {
+        TransferenciaResponseDTO dto = transferenciaService.transferencia(transferenciaRequestDTO);
+        return ResponseEntity.ok(dto);
+    }
 }
