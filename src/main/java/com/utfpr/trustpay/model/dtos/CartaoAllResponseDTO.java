0da638f809b5ff48cf0ebe7cc87961cd74329b5b
Bruno Piovan <bpiovan90@gmail.com>
2025-10-06 14:03:03 -0300

    alteracao para cartao retornar id junto do dto

--- src/main/java/com/utfpr/trustpay/model/dtos/CartaoAllResponseDTO.java
+++ src/main/java/com/utfpr/trustpay/model/dtos/CartaoAllResponseDTO.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CartaoAllResponseDTO {
 
+    private Long id;
     private String numero;
     private String nomeTitular;
     private String validade;
--- src/main/java/com/utfpr/trustpay/repository/CartaoRepository.java
+++ src/main/java/com/utfpr/trustpay/repository/CartaoRepository.java
@@ -2,8 +2,6 @@ package com.utfpr.trustpay.repository;
 
 import com.utfpr.trustpay.model.Cartao;
 import com.utfpr.trustpay.model.dtos.CartaoAllResponseDTO;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,13 +11,9 @@ import java.util.List;
 
 @Repository
 public interface CartaoRepository  extends JpaRepository<Cartao,Long> {
-    @Query("SELECT new com.utfpr.trustpay.model.dtos.CartaoAllResponseDTO(" +
-            "c.numero, c.nomeTitular, c.validade, c.cvv, c.tipo, c.bloqueado) " +
-            "FROM Cartao c WHERE c.usuario.id = :userId")
-    Page<CartaoAllResponseDTO> findAllByUsuarioId(@Param("userId") Long userId, Pageable pageable);
 
     @Query("SELECT new com.utfpr.trustpay.model.dtos.CartaoAllResponseDTO(" +
-            "c.numero, c.nomeTitular, c.validade, c.cvv, c.tipo, c.bloqueado) " +
+            "c.id, c.numero, c.nomeTitular, c.validade, c.cvv, c.tipo, c.bloqueado) " +
             "FROM Cartao c WHERE c.usuario.id = :userId")
     List<CartaoAllResponseDTO> findAllByUsuarioId(@Param("userId") Long userId);
 
